40569dc5f42453c07c447ef9aaaa5cbea54ae8d4
Felipe Betts <felipe.a.betts@gmail.com>
2024-06-10 22:39:25 -0300

    feat: create breadcrumbs

--- src/components/works/work-details.tsx
+++ src/components/works/work-details.tsx
@@ -1,7 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { IoCaretBack } from 'react-icons/io5'
+import Breadcrumbs from '../breadcrumbs'
 
-const WorkDetails = () => {
-  return <div>WorkDetails</div>
+interface Props {
+  children: React.ReactNode
+  sub: string
+  title: string
+}
+
+const WorkDetails: React.FC<Props> = ({ children, sub, title }) => {
+  return (
+    <div>
+      <header className="w-full max-w-[var(--max-w)] min-h-[30vh] bg-black flex flex-col justify-end mx-auto">
+        <div className="p-6">
+          <Breadcrumbs />
+          <h2 className="text-2xl mt-6 mb-4">{sub}</h2>
+          <h1 className="text-6xl">{title}</h1>
+        </div>
+      </header>
+      <main className="w-full bg-[var(--white)] text-black">
+        <article className="w-full max-w-[var(--max-w)] mx-auto p-6">
+          {children}
+        </article>
+      </main>
+    </div>
+  )
 }
 
 export default WorkDetails
