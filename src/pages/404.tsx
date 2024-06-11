6d22cd8abff771fa9b234bf9f74d55cfd135df0c
Felipe Betts <felipe.a.betts@gmail.com>
2024-06-10 22:41:50 -0300

    fix: not found page padding

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <main className="min-h-[75vh] w-full flex flex-col gap-8 items-center justify-center">
+    <main className="min-h-[75vh] w-full flex flex-col gap-8 items-center justify-center py-6 px-4">
       <h1 className="mb-6 text-4xl xl:text-5xl">Page Not Found</h1>
       <video src="/videos/drink-water.mp4" autoPlay loop></video>
       <h2 className="mb-2 text-2xl xl:text-3xl -tracking-tight">
