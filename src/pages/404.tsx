c849f73a2c280ac6e45cdeaea36878c3606231b7
Felipe Betts <felipe.a.betts@gmail.com>
2024-06-10 22:43:52 -0300

    fix: 404 video options

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,13 @@ const NotFound = () => {
   return (
     <main className="min-h-[75vh] w-full flex flex-col gap-8 items-center justify-center py-6 px-4">
       <h1 className="mb-6 text-4xl xl:text-5xl">Page Not Found</h1>
-      <video src="/videos/drink-water.mp4" autoPlay loop></video>
+      <video
+        src="/videos/drink-water.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
       <h2 className="mb-2 text-2xl xl:text-3xl -tracking-tight">
         <span>
           Return to the{' '}
