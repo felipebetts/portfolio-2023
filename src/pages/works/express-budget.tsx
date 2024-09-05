1b4cefe756ea2af877223890d4d55b934c4c11e9
Felipe Betts <felipe.a.betts@gmail.com>
2024-09-04 21:34:43 -0300

    fix: escape unescaped entities

--- src/pages/works/express-budget.tsx
+++ src/pages/works/express-budget.tsx
@@ -40,12 +40,12 @@ const ExpressBudget = () => {
           This web application is a home building budget simulator. I was asked
           by a brazilian realstate company to make this web application, in
           order to simplify the budgeting process for their clients. Before this
-          webapp went live, the company's budgeting department was struggling
-          with clients that asked for house budgets, only to find out that the
-          budget was too high for them, generating work that didn't turned to
-          revenue. The main goal of the app was to generate leads with loads of
-          data about the clients, and to optimize the budgeting department's
-          time.
+          webapp went live, the company&apos;s budgeting department was
+          struggling with clients that asked for house budgets, only to find out
+          that the budget was too high for them, generating work that
+          didn&apos;t turned to revenue. The main goal of the app was to
+          generate leads with loads of data about the clients, and to optimize
+          the budgeting department&apos;s time.
         </p>
         <LazyImage
           height={600}
@@ -55,7 +55,7 @@ const ExpressBudget = () => {
           className="my-6"
         />
         <p className="my-2">
-          Based on the user's input of their income and other financial
+          Based on the user&apos;s input of their income and other financial
           parameters, it performs a mortgage simulation. With that, it generates
           the average value of the mortgage, wich will be used as the maximum
           budget for the house construction.
@@ -83,9 +83,9 @@ const ExpressBudget = () => {
         />
         <p className="my-2">
           At the end, the user is asked for contact information in order to have
-          access to the final report. By my client's request, this data is sent
-          to Bitrix24, via their own api, and stored as a lead. Finally the user
-          is able to see a resumed report of their choices and final budget
+          access to the final report. By my client&apos;s request, this data is
+          sent to Bitrix24, via their own api, and stored as a lead. Finally the
+          user is able to see a resumed report of their choices and final budget
           generated.
         </p>
       </section>
